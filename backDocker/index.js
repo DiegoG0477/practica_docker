17356eec7a8781a84ab439fb168f916ec603722c
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-11-07 22:23:10 -0600

    update

--- backDocker/index.js
+++ backDocker/index.js
@@ -2,50 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql2');
-const dotenv = require("dotenv");
-dotenv.config();
+const dotenv = require("dotenv")
+dotenv.config()
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const dbConfig = {
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD, 
   database: process.env.DATABASE,
   port: 3306
-};
+});
 
-// Función para manejar la conexión con reintentos
-function connectWithRetry() {
-  const db = mysql.createConnection(dbConfig);
-
-  db.connect((err) => {
-    if (err) {
-      console.error('Error conectando a la base de datos:', err);
-      // Reintenta la conexión después de 5 segundos si falla
-      setTimeout(connectWithRetry, 5000);
-    } else {
-      console.log('Conectado a la base de datos MySQL');
-    }
-  });
-
-  // Manejo de errores en la conexión
-  db.on('error', (err) => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.log('Conexión a MySQL perdida. Reintentando...');
-      connectWithRetry();
-    } else {
-      throw err;
-    }
-  });
-
-  return db;
-}
-
-// Crea la conexión de base de datos con reintento
-const db = connectWithRetry();
+db.connect((err) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+  } else {
+    console.log('Conectado a la base de datos MySQL');
+  }
+});
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
